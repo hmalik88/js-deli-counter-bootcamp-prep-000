877f4abb47ce506046ef11b2fdcca287a3c1aaab
Hassan Malik <hbmalik88@gmail.com>
2018-09-12 17:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-var katzDeli = [];
+
 function takeANumber (katzDeliLine, personname) {
   katzDeliLine.push(personname);
   return `Welcome, ${personname}. You are number ${katzDeliLine.length} in line.`;
 }
+
 function nowServing (katzDeliLine) {
   var removedname;
-  if (katzDeliLine.length===0) {
-    return "There is nobody waiting to be served!"
-  }
-   else {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
      removedname = katzDeliLine[0];
      katzDeliLine.shift();
      return `Currently serving ${removedname}.`;
 }
 }
+
 function currentLine(line) { 
-  if (!line.length) 
-  { return "The line is currently empty." } 
-  const numbersAndNames = [];
-  for (let i = 0, l = line.length; i < l; i++) 
-  { numbersAndNames.push(`${i + 1}. ${line[i]}`) } 
-  return `The line is currently: ${numbersAndNames.join(', ')}`
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  } 
+  var numbersAndNames = [];
+  for (let i = 0; i < line.length; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`);
+  } 
+  return `The line is currently: ${numbersAndNames.join(', ')}`;
   }
